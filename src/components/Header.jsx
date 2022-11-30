d1154f10d9cdb3b4b9595f6975e6e8ba5a12eb9a
Camber62 <Camber62@mail.ru>
2022-11-30 19:33:13 +0300

    Lesson

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,19 +10,19 @@ const Header = () => {
 
             <Navbar collapseOnSelect expand="lg" className="bgNavbar sticky-top">
                 <Container className="container-fluid">
-                    <Navbar.Brand href="responsev-navbar-nav" >Web</Navbar.Brand>
+                    <Navbar.Brand href="responsev-navbar-nav">Web</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsev-navbar-nav"/>
                     <Navbar.Collapse id="responsev-navbar-nav">
 
                         <Nav defaultActiveKey="/home">
-                            <div className='d-flex'>
+                            <div className=''>
                                 <Nav.Link href="#catalog">КАТАЛОГ</Nav.Link>
                                 <Nav.Link href="#AboutUs" eventKey="link-1">О НАС</Nav.Link>
                                 <Nav.Link href="#blog" eventKey="link-2">БЛОГ</Nav.Link>
                                 <Nav.Link href="#form" eventKey="link-3">ДОСТАВКА И ОПЛАТА</Nav.Link>
                                 <Nav.Link href="#form" eventKey="link-4">ИНДМВИДУАЛЬНЫЙ ЗАКАЗ</Nav.Link>
                             </div>
-                            <Nav.Link href="#Contacts" eventKey="link-5">КОНТАКТЫ</Nav.Link>
+                            <div><Nav.Link href="#Contacts" eventKey="link-5">КОНТАКТЫ</Nav.Link></div>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
