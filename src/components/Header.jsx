44717025bf852dc4076f17335c942e946dbca533
Camber62 <Camber62@mail.ru>
2022-11-30 19:01:32 +0300

    Lesson

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import {Navbar,Nav} from "react-bootstrap";
+import {Navbar, Nav} from "react-bootstrap";
 
 
 const Header = () => {
@@ -9,23 +9,23 @@ const Header = () => {
 
 
             <Navbar collapseOnSelect expand="lg" className="bgNavbar sticky-top">
-            <Container className="container-fluid">
-                <Navbar.Brand href="#" className="d-inline-block align-top">Web</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsev-navbar-nav"/>
-                <Navbar.Collapse className="justify-content-around" id="responsev-navbar-nav" >
+                <Container className="container-fluid">
+                    <Navbar.Brand href="responsev-navbar-nav" >Web</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsev-navbar-nav"/>
+                    <Navbar.Collapse id="responsev-navbar-nav">
 
-                <Nav defaultActiveKey="/home">
-                        <Nav.Link href="#catalog">КАТАЛОГ</Nav.Link>
-                        <Nav.Link href="#AboutUs" eventKey="link-1">О НАС</Nav.Link>
-                        <Nav.Link href="#blog" eventKey="link-2">БЛОГ</Nav.Link>
-                        <Nav.Link href="#form" eventKey="link-3">ДОСТАВКА И ОПЛАТА</Nav.Link>
-                        <Nav.Link href="#form" eventKey="link-4">ИНДМВИДУАЛЬНЫЙ ЗАКАЗ</Nav.Link>
-                    <div className="me-2">
-                        <Nav.Link href="#Contacts" eventKey="link-5">КОНТАКТЫ</Nav.Link>
-                    </div>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
+                        <Nav defaultActiveKey="/home">
+                            <div className='d-flex'>
+                                <Nav.Link href="#catalog">КАТАЛОГ</Nav.Link>
+                                <Nav.Link href="#AboutUs" eventKey="link-1">О НАС</Nav.Link>
+                                <Nav.Link href="#blog" eventKey="link-2">БЛОГ</Nav.Link>
+                                <Nav.Link href="#form" eventKey="link-3">ДОСТАВКА И ОПЛАТА</Nav.Link>
+                                <Nav.Link href="#form" eventKey="link-4">ИНДМВИДУАЛЬНЫЙ ЗАКАЗ</Nav.Link>
+                            </div>
+                            <Nav.Link href="#Contacts" eventKey="link-5">КОНТАКТЫ</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </>
     )
