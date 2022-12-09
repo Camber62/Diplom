b79e3e17d2a6d1773c7ab4cd6c41196d00f9e9dc
Camber62 <Camber62@mail.ru>
2022-12-09 08:57:45 +0300

    work

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ server.post('/send-order', async (req,res)=>{
 <b>Товары:</b>
 `
     for (let i = 0; i < req.body.basket.length; i++) {
-        message += `${i + 1}. ${req.body.basket[i].name}`
+        message += ` \n ${i + 1}. ${req.body.basket[i].name}`
     }
 
     await telegram.sendToPrivateChat(message)
